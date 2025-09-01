07fe7f9fa4c4776121eb44c6354a9ed585ce46d6
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-09-01 20:29:42 -0300

    solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,59 @@
 from app.split_integer import split_integer
+import pytest
+
+VARS: str = "first_value,second_value,result"
+TESTES_VALUES: list = [
+    (8, 1, [8]),
+    (6, 2, [3, 3]),
+    (17, 4, [4, 4, 4, 5]),
+    (32, 6, [5, 5, 5, 5, 6, 6])
+]
+
+TESTES_VALUES2: list = [
+    (8, 1, [8]),
+    (10, 1, [10]),
+    (7, 1, [7]),
+    (101, 1, [101])
+]
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+@pytest.mark.parametrize(VARS, TESTES_VALUES)
+def test_sum_of_the_parts_should_be_equal_to_value(
+    first_value: int,
+    second_value: int,
+    result: list
+) -> None:
+    assert split_integer(first_value, second_value) == result
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+@pytest.mark.parametrize(VARS, TESTES_VALUES)
+def test_should_split_into_equal_parts_when_value_divisible_by_parts(
+    first_value: int,
+    second_value: int,
+    result: list
+) -> None:
+    assert second_value == len(result)
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+@pytest.mark.parametrize(VARS, TESTES_VALUES2)
+def test_should_return_part_equals_to_value_when_split_into_one_part(
+    first_value: int,
+    second_value: int,
+    result: list
+) -> None:
+    assert split_integer(first_value, second_value) == result
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+@pytest.mark.parametrize(VARS, TESTES_VALUES)
+def test_parts_should_be_sorted_when_they_are_not_equal(
+    first_value: int,
+    second_value: int,
+    result: list
+) -> None:
+    assert split_integer(first_value, second_value) == sorted(result)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    expected = [0, 0, 1]
+    actual = split_integer(1, 3)
+    assert expected == actual
