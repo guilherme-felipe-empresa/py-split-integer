8d5c700253e09ccfe2adc018d001602663d4c071
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-09-01 20:51:22 -0300

    resolution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,8 @@ TESTES_VALUES: list = [
     (8, 1, [8]),
     (6, 2, [3, 3]),
     (17, 4, [4, 4, 4, 5]),
-    (32, 6, [5, 5, 5, 5, 6, 6])
+    (32, 6, [5, 5, 5, 5, 6, 6]),
+    (1.2, 6, [0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
 ]
 
 TESTES_VALUES2: list = [
@@ -26,13 +27,10 @@ def test_sum_of_the_parts_should_be_equal_to_value(
     assert split_integer(first_value, second_value) == result
 
 
-@pytest.mark.parametrize(VARS, TESTES_VALUES)
-def test_should_split_into_equal_parts_when_value_divisible_by_parts(
-    first_value: int,
-    second_value: int,
-    result: list
-) -> None:
-    assert second_value == len(result)
+def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+    assert len(split_integer(5, 8)) == 8
+    assert len(split_integer(5, 5)) == 5
+    assert len(split_integer(5, 1)) == 1
 
 
 @pytest.mark.parametrize(VARS, TESTES_VALUES2)
@@ -50,10 +48,8 @@ def test_parts_should_be_sorted_when_they_are_not_equal(
     second_value: int,
     result: list
 ) -> None:
-    assert split_integer(first_value, second_value) == sorted(result)
+    assert split_integer(first_value, second_value) == result
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    expected = [0, 0, 1]
-    actual = split_integer(1, 3)
-    assert expected == actual
+    assert split_integer(1, 3) == [0, 0, 1]
